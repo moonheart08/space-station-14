14b5f1611d3be30c8f3e57cdd7be1422237324c2
moonheart08 <moonheart08@users.noreply.github.com>
2021-11-28 18:47:34 -0600

    changing permissions for everything ever

--- Content.Shared/Administration/AdminFlags.cs
+++ Content.Shared/Administration/AdminFlags.cs
@@ -66,6 +66,16 @@ namespace Content.Shared.Administration
         Logs = 1 << 9,
 
         /// <summary>
+        ///     Lets you use query commands, i.e. forall
+        /// </summary>
+        Query = 1 << 10,
+
+        /// <summary>
+        ///     Lets you manipulate the round, i.e. startround
+        /// </summary>
+        Round = 1 << 11,
+
+        /// <summary>
         ///     Dangerous host permissions like scsi.
         /// </summary>
         Host = 1u << 31,
