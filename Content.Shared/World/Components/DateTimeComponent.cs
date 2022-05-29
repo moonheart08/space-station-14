749eba7b147cbde761e83e9815ec94860528485a
moonheart08 <moonheart08@users.noreply.github.com>
2022-05-29 00:11:03 -0500

    save work.

---
+++ Content.Shared/World/Components/DateTimeComponent.cs
@@ -0,0 +1,24 @@
+﻿using Robust.Shared.Serialization;
+
+namespace Content.Shared.World.Components;
+
+/// <summary>
+/// Contains orbital period, day/night, and other information about the map's time of day.
+/// </summary>
+public abstract class DateTimeComponent : Component
+{
+    [DataField("")]
+    public TimeSpan OrbitalPeriod = default!;
+}
+
+/// <summary>
+/// Contains network state for DateTimeComponent.
+/// </summary>
+[NetSerializable, Serializable]
+public sealed class DateTimeComponentState : ComponentState
+{
+    public DateTimeComponentState(DateTimeComponent component)
+    {
+
+    }
+}
---
+++ Content.Shared/World/Systems/DateTimeSystem.cs
@@ -0,0 +1,21 @@
+﻿using Content.Shared.World.Components;
+using Robust.Shared.Timing;
+
+namespace Content.Shared.World.Systems;
+
+/// <summary>
+/// Handles the local time for a map. This means orbital period, day/night period, all that jazz.
+/// </summary>
+public abstract class DateTimeSystem : EntitySystem
+{
+    [Dependency] protected readonly IGameTiming _gameTiming = default!;
+
+    /// <inheritdoc/>
+    public override void Update(float frameTime)
+    {
+        foreach (var dateTime in EntityQuery<DateTimeComponent>())
+        {
+
+        }
+    }
+}
