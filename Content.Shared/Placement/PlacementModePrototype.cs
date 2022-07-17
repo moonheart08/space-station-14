30c46e44341b0707f0e645b12d96d8b5f0a6ea3a
moonheart08 <moonheart08@users.noreply.github.com>
2022-07-16 23:10:11 -0500

    i started but then i died inside

---
+++ Content.Shared/Placement/PlacementModePrototype.cs
@@ -0,0 +1,17 @@
+﻿using Robust.Shared.Prototypes;
+
+namespace Content.Shared.Placement;
+
+/// <summary>
+/// This is a prototype for...
+/// </summary>
+[Prototype("placementMode")]
+public sealed class PlacementModePrototype : IPrototype
+{
+    /// <inheritdoc/>
+    [IdDataField]
+    public string ID { get; } = default!;
+
+    [DataField("name", required: true)]
+    public string Name { get; } = default!;
+}
---
+++ Content.Shared/Placement/SharedPlacementSystem.cs
@@ -0,0 +1,17 @@
+﻿namespace Content.Shared.Placement;
+
+/// <summary>
+/// This handles...
+/// </summary>
+public abstract class SharedPlacementSystem : EntitySystem
+{
+    /// <inheritdoc/>
+    public override void Initialize()
+    {
+
+    }
+
+    public bool
+}
+
+public enum
